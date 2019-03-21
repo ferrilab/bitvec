d4580274cb9767306b92f1b77328a95442e3f134
myrrlyn <self@myrrlyn.dev>
2019-03-21 12:58:44 -0600

    Documentation pass and code reuse refactor

--- src/cursor.rs
+++ src/cursor.rs
@@ -20,11 +20,11 @@ use super::bits::{
 	Bits,
 };
 
-/// Traverses an element from `MSb` to `LSb`.
+/// Traverses an element from `MSbit` to `LSbit`.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct BigEndian;
 
-/// Traverses an element from `LSb` to `MSb`.
+/// Traverses an element from `LSbit` to `MSbit`.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct LittleEndian;
 
@@ -37,28 +37,60 @@ pub struct LittleEndian;
 electrical position, `BitPos`.
 **/
 pub trait Cursor {
+	/// Name of the cursor type, for use in text display.
 	const TYPENAME: &'static str;
 
 	/// Translate a semantic bit index into an electrical bit position.
 	///
 	/// # Parameters
 	///
-	/// - `cursor`: The semantic bit value. This must be in the domain
-	///   `0 .. T::SIZE`.
+	/// - `cursor`: The semantic bit value.
 	///
 	/// # Returns
 	///
 	/// - A concrete position. This value can be used for shifting and masking
-	///   to extract a bit from an element.
+	///   to extract a bit from an element. This must be in the domain
+	///   `0 .. T::SIZE`.
 	///
 	/// # Type Parameters
 	///
 	/// - `T: Bits`: The storage type for which the position will be calculated.
 	///
+	/// # Invariants
+	///
+	/// The function **must** be *total* for the domain `.. T::SIZE`. All values
+	/// in this domain are valid indices that the library will pass to it, and
+	/// which this function must satisfy.
+	///
+	/// The function **must** be *bijective* over the domain `.. T::SIZE`. All
+	/// input values in this domain must have one and only one correpsonding
+	/// output, which must also be in this domain.
+	///
+	/// The function *may* support input in the domain `T::SIZE ..`. The library
+	/// will not produce any values in this domain as input indices. The
+	/// function **must not** produce output in the domain `T::SIZE ..`. It must
+	/// choose between panicking, or producing an output in `.. T::SIZE`. The
+	/// reduction in domain from `T::SIZE ..` to `.. T::SIZE` removes the
+	/// requirement for inputs in `T::SIZE ..` to have unique outputs in
+	/// `.. T::SIZE`.
+	///
+	/// This function **must** be *pure*. Calls which have the same input must
+	/// produce the same output. This invariant is only required to be upheld
+	/// for the lifetime of all data structures which use an implementor. The
+	/// behavior of the function *may* be modified after all existing dependent
+	/// data structures are destroyed and before any new dependent data
+	/// structures are created.
+	///
+	/// # Non-Invariants
+	///
+	/// This function is *not* required to be stateless. It *may* refer to
+	/// immutable global state, subject to the purity requirement on lifetimes.
+	///
 	/// # Safety
 	///
-	/// This function requires that `cursor` be in the domain `0 .. T::SIZE`.
-	/// Implementors must check this themselves.
+	/// This function requires that the output be in the domain `.. T::SIZE`.
+	/// Implementors must uphold this themselves. Outputs in the domain
+	/// `T::SIZE ..` will induce panics elsewhere in the library.
 	fn at<T: Bits>(cursor: BitIdx) -> BitPos;
 }
 
@@ -67,7 +99,7 @@ impl Cursor for BigEndian {
 
 	/// Maps a semantic count to a concrete position.
 	///
-	/// `BigEndian` order moves from `MSb` first to `LSb` last.
+	/// `BigEndian` order moves from `MSbit` first to `LSbit` last.
 	fn at<T: Bits>(cursor: BitIdx) -> BitPos {
 		assert!(
 			*cursor < T::SIZE,
@@ -84,7 +116,7 @@ impl Cursor for LittleEndian {
 
 	/// Maps a semantic count to a concrete position.
 	///
-	/// `LittleEndian` order moves from `LSb` first to `LSb` last.
+	/// `LittleEndian` order moves from `LSbit` first to `MSbit` last.
 	fn at<T: Bits>(cursor: BitIdx) -> BitPos {
 		assert!(
 			*cursor < T::SIZE,
