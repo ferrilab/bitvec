7f7fe9a3b2ba57332cb360ff1028ba67951d4afe
myrrlyn <self@myrrlyn.dev>
2019-08-30 16:08:17 -0600

    Add more tests to `BitSlice`

--- src/slice/tests.rs
+++ src/slice/tests.rs
@@ -117,6 +117,28 @@ fn from_slice() {
 	assert_eq!(bs.count_zeros(), 64);
 
 	}
+
+	let good_slice: &[u8] = unsafe {
+		core::slice::from_raw_parts(
+			core::ptr::NonNull::<u8>::dangling().as_ptr(),
+			BitPtr::<u8>::MAX_ELTS,
+		)
+	};
+	let bs = BitSlice::<BigEndian, _>::from_slice(good_slice);
+	assert_eq!(bs.len(), BitPtr::<u8>::MAX_INDX);
+}
+
+#[cfg(not(miri))]
+#[test]
+#[should_panic]
+fn from_slice_assertions() {
+	let evil_slice: &[u8] = unsafe {
+		core::slice::from_raw_parts(
+			core::ptr::NonNull::<u8>::dangling().as_ptr(),
+			BitPtr::<u8>::MAX_ELTS + 1,
+		)
+	};
+	let _ = BitSlice::<BigEndian, _>::from_slice(evil_slice);
 }
 
 #[test]
@@ -173,7 +195,13 @@ fn first() {
 	let elt = 128u8;
 	let bs = BitSlice::<BigEndian, _>::from_element(&elt);
 	assert_eq!(bs.first(), Some(true));
+
 	assert!(BitSlice::<BigEndian, u8>::empty().first().is_none());
+	assert!(BitSlice::<BigEndian, u16>::empty().first().is_none());
+	assert!(BitSlice::<BigEndian, u32>::empty().first().is_none());
+
+	#[cfg(target_pointer_width = "64")]
+	assert!(BitSlice::<BigEndian, u64>::empty().first().is_none());
 }
 
 #[test]
@@ -206,4 +234,177 @@ fn split_first_mut() {
 	assert_eq!(rest.len(), 15);
 	rest.set(0, true);
 	assert_eq!(elt, 0xC003);
+}
+
+#[test]
+fn split_last() {
+	assert!(BitSlice::<BigEndian, u8>::empty().split_last().is_none());
+
+	let elt = 129u8;
+	let bs = BitSlice::<BigEndian, _>::from_element(&elt);
+	let (tail, rest) = bs.split_last().unwrap();
+	assert!(tail);
+	assert!(rest[0]);
+	assert_eq!(rest.len(), 7);
+}
+
+#[test]
+fn split_last_mut() {
+	assert!(BitSlice::<BigEndian, u8>::empty_mut().split_first_mut().is_none());
+
+	let mut elt = 0x0001u16;
+	let bs = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+	let (tail, rest) = bs.split_last_mut().unwrap();
+	assert!(tail);
+	assert_eq!(rest.len(), 15);
+	rest.set(0, true);
+	assert_eq!(elt, 0x8001);
+
+	let bs = BitSlice::<LittleEndian, _>::from_element_mut(&mut elt);
+	let (tail, rest) = bs.split_last_mut().unwrap();
+	assert!(tail); // set in the previous block
+	assert_eq!(rest.len(), 15);
+	assert!(rest[0]);
+	rest.set(0, false);
+	assert_eq!(elt, 0x8000);
+}
+
+#[test]
+fn last() {
+	let elt = 1u8;
+	let bs = BitSlice::<BigEndian, _>::from_element(&elt);
+	assert_eq!(bs.last(), Some(true));
+
+	assert!(BitSlice::<BigEndian, u8>::empty().last().is_none());
+	assert!(BitSlice::<BigEndian, u16>::empty().last().is_none());
+	assert!(BitSlice::<BigEndian, u32>::empty().last().is_none());
+
+	#[cfg(target_pointer_width = "64")]
+	assert!(BitSlice::<BigEndian, u64>::empty().last().is_none());
+}
+
+#[test]
+fn set_get_at() {
+	let mut elt = 0u8;
+	let bs = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+
+	assert!(bs.get(8).is_none());
+	assert_eq!(bs.get(0), Some(false));
+	bs.set(0, true);
+	assert_eq!(bs.get(0), Some(true));
+
+	#[cfg(feature = "std")]
+	assert!(std::panic::catch_unwind(|| {
+		let mut elt = 0u8;
+		let bs = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+		bs.set(9, true);
+	}).is_err());
+
+	*bs.at(1) = true;
+	assert!(bs.get(1).unwrap());
+}
+
+#[test]
+fn as_ptr() {
+	let mut elt = 0u8;
+	let eltptr = &mut elt as *mut u8;
+	let bs = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+
+	assert_eq!(bs.as_ptr(), eltptr as *const u8);
+	assert_eq!(bs.as_mut_ptr(), eltptr);
+}
+
+#[test]
+fn swap_reverse() {
+	let mut elt = 0b1111_0000u8;
+	let bs = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+
+	assert!(bs[3]);
+	assert!(!bs[4]);
+	bs.swap(3, 4);
+	assert!(!bs[3]);
+	assert!(bs[4]);
+
+	bs.reverse();
+	assert_eq!(elt, 0b0001_0111);
+
+	//  check that `reverse` correctly handles odd-length slices
+	let mut elt = 0b101_0_010_1u8;
+	let bs = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+	let bs = bs.split_last_mut().unwrap().1;
+	assert_eq!(bs.len(), 7);
+	bs.reverse();
+	assert_eq!(elt, 0b010_0_101_1);
+}
+
+#[test]
+fn iter() {}
+
+#[test]
+fn windows() {
+	let elt = 0x81u8;
+	let bs = BitSlice::<BigEndian, _>::from_element(&elt);
+	let mut windows = bs.windows(7);
+
+	let one = windows.next().unwrap();
+	assert_eq!(one.len(), 7);
+	assert_eq!(one, &bs[.. 7]);
+
+	let two = windows.next().unwrap();
+	assert_eq!(two.len(), 7);
+	assert_eq!(two, &bs[1 ..]);
+
+	assert!(windows.next().is_none());
+}
+
+#[test]
+fn chunks() {
+	let elt = 0b100_010_01u8;
+	let bs = BitSlice::<BigEndian, _>::from_element(&elt);
+
+	let mut chunks = bs.chunks(3);
+	assert_eq!(chunks.next().unwrap(), &bs[0 .. 3]);
+	assert_eq!(chunks.next().unwrap(), &bs[3 .. 6]);
+	assert_eq!(chunks.next().unwrap(), &bs[6 .. 8]);
+	assert!(chunks.next().is_none());
+
+	let mut chunks = bs.chunks(3);
+	assert_eq!(chunks.next_back().unwrap(), &bs[6 .. 8]);
+	assert_eq!(chunks.next_back().unwrap(), &bs[3 .. 6]);
+	assert_eq!(chunks.next_back().unwrap(), &bs[0 .. 3]);
+	assert!(chunks.next_back().is_none());
+
+	//  chunks_exact
+
+	let elt = 0b100_010_01u8;
+	let bs = BitSlice::<BigEndian, _>::from_element(&elt);
+
+	let mut chunks = bs.chunks_exact(3);
+	assert_eq!(chunks.next().unwrap(), &bs[0 .. 3]);
+	assert_eq!(chunks.next().unwrap(), &bs[3 .. 6]);
+	assert!(chunks.next().is_none());
+	assert_eq!(chunks.remainder(), &bs[6 .. 8]);
+
+	//  chunks_mut
+
+	let mut elt = 0b0100_1011u8;
+	let bits = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+
+	let mut chunks = bits.chunks_mut(3);
+	chunks.next().unwrap().set(2, true);
+	chunks.next().unwrap().set(2, true);
+	chunks.next().unwrap().set(1, false);
+	assert!(chunks.next().is_none());
+
+	assert_eq!(elt, 0b011_011_10);
+
+	//  chunks_exact_mut
+
+	let mut elt = 0b010_010_11u8;
+	let bits = BitSlice::<BigEndian, _>::from_element_mut(&mut elt);
+
+	let mut chunks = bits.chunks_exact_mut(3);
+	chunks.next().unwrap().set(2, true);
+	chunks.next().unwrap().set(2, true);
+	assert!(chunks.next().is_none());
 }
