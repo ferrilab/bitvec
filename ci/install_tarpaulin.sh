696eb67c51b300b7bf1c3e128b6fe2475ad16d1b
myrrlyn <self@myrrlyn.dev>
2020-07-05 19:32:54 -0400

    Use prebuilt tarpaulin images

--- ci/install_tarpaulin.sh
+++ ci/install_tarpaulin.sh
@@ -3,5 +3,5 @@
 set -ex
 
 if [[ ! -z $COVERAGE ]]; then
-    cargo +nightly install cargo-tarpaulin -f
+    bash <(curl https://raw.githubusercontent.com/xd009642/tarpaulin/master/travis-install.sh)
 fi
